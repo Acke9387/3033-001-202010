c12d39e3132a9bfbc724ba1a6fadd8b4a53060be
Ackerman, Adam P <adamackrmn@gmail.com>
2020-09-03 18:17:05 -0500

    Completed the more advanced UML for student with try catch

--- ReviewProblems/MoreDifficultStudentExample/Program.cs
+++ ReviewProblems/MoreDifficultStudentExample/Program.cs
@@ -6,7 +6,25 @@ namespace MoreDifficultStudentExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Student myStudent = new Student(5, "Adam", "Ackerman", 1000);
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            try
+            {
+                myStudent.MakePayment(-500);
+                Console.WriteLine(myStudent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
         }
     }
 }
---
+++ ReviewProblems/MoreDifficultStudentExample/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreDifficultStudentExample
+{
+    class Student
+    {
+        // hold alt + shift, use arrow keys to select lines
+        // let go of alt + shit and type
+        public int SoonerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+        private double BursarBalance;
+
+        /// <summary>
+        /// Default constructor for the Student class
+        /// </summary>
+        public Student()
+        {
+            SoonerId = -1;
+            FirstName = string.Empty;
+            LastName = "";
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 10000;
+        }
+
+        public Student(int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerId = id;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+        }
+
+        public void MakePayment(double amount)
+        {
+            if (amount > 0)
+            {
+                BursarBalance = BursarBalance - amount;
+            }
+            else
+            {
+                throw new Exception("INVALID PAYMENT AMOUNT");
+            }
+        }
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{FirstName} {LastName} ({SoonerId}) has a {GPA.ToString("N2")} GPA and owes {BursarBalance.ToString("C")}";
+
+            if(IsOnProbation == true)
+            {
+                result += "\nIs on Probation";
+            }
+            else
+            {
+                result += "\nIs not on Probation";
+            }
+
+            return result;
+        }
+    }
+}
