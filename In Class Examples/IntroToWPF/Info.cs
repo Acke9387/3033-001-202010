20499a15422f55bc6b0812f6ffe562db468231e5
Ackerman, Adam P <adamackrmn@gmail.com>
2020-09-08 18:06:49 -0500

    Finished the inclass example of intro to WPF

---
+++ In Class Examples/IntroToWPF/Info.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroToWPF
+{
+    class Info
+    {
+        public string Name { get; set; }
+        public string FavoriteColor { get; set; }
+
+        public Info()
+        {
+            Name = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+    }
+}
--- In Class Examples/IntroToWPF/MainWindow.xaml.cs
+++ In Class Examples/IntroToWPF/MainWindow.xaml.cs
@@ -23,6 +23,53 @@ namespace IntroToWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            txtFavoriteColor.Text =  string.Empty;
+            txtName.Clear();
+            btnClickMe.IsEnabled = false;
+            // Getting the value from the textbox
+            //var x = txtFavoriteColor.Text;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var response = MessageBox.Show("You clicked me !", "Enter title here", MessageBoxButton.YesNoCancel);
+            Info i = new Info();
+            i.FavoriteColor = txtFavoriteColor.Text;
+            i.Name = txtName.Text;
+            string message = $"Hey {i.Name}, that's cool your favorite color is {i.FavoriteColor}!";
+            MessageBox.Show(message, "!~Welcome to MIS 3033~!");
+        }
+
+        private void txtFavoriteColor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //MessageBox.Show("You changed me");
+        }
+
+        private void txt_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if(ShouldWeEnableTheButton() == true)
+            //{
+            //    btnClickMe.IsEnabled = true;
+            //}
+            //else
+            //{
+            //    btnClickMe.IsEnabled = false;
+            //}
+
+            btnClickMe.IsEnabled = ShouldWeEnableTheButton();
+        }
+
+        private bool ShouldWeEnableTheButton()
+        {
+            bool result = false;
+            if (txtFavoriteColor.Text != string.Empty
+                && txtName.Text != string.Empty)
+            {
+                result = true;
+            }
+
+            return result;
         }
     }
 }
