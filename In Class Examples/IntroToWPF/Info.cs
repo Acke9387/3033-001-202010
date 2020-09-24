35767a1c7ba13cd54ef9db156e38b3815cbb02cb
Ackerman, Adam P <adamackrmn@gmail.com>
2020-09-24 18:13:32 -0500

    Created a project for a JSON example

--- In Class Examples/IntroToWPF/Info.cs
+++ In Class Examples/IntroToWPF/Info.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace IntroToWPF
 {
-    class Info
+    public class Info
     {
         public string Name { get; set; }
         public string FavoriteColor { get; set; }
