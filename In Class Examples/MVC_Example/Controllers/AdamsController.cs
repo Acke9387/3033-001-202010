c80bd44adb3aad0d828bb190c6c21a02a2701dcc
Ackerman, Adam P <adamackrmn@gmail.com>
2020-11-10 17:58:34 -0600

    Added a new controler from scratch and went over entity framework

---
+++ In Class Examples/MVC_Example/Controllers/AdamsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Example.Controllers
+{
+    public class AdamsController : Controller
+    {
+        // GET: Adams
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        
+    }
+}
